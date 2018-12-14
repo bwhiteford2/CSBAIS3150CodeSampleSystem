bebddc8e8fc493f1949fd680f521708c14ea8dab
bwhiteford2 <31717332+bwhiteford2@users.noreply.github.com>
2018-12-13 19:51:35 -0700

    process sale is broken :(

--- final proj/ABCHardware/ABCHardwareSite/ABCHardwareSite/ProcessSale.aspx
+++ final proj/ABCHardware/ABCHardwareSite/ABCHardwareSite/ProcessSale.aspx
@@ -8,6 +8,30 @@
     <h1 class="jumbotron">Process Sale</h1>
 
     <h3>Incomplete :(</h3>
+
+    <div class="row form-body">
+        <div class="col-md-6">
+            <asp:Label ID="MessageBox" runat="server" Text=""></asp:Label>
+            <br />
+
+            <asp:Label ID="ItemCodeLbl" runat="server" Text="Item Code:"></asp:Label>
+            <asp:TextBox ID="ItemCodeTextBox" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="ReqItemCode" runat="server" ErrorMessage="Item code is required" ControlToValidate="ItemCodeTextBox" Display="Dynamic"></asp:RequiredFieldValidator>
+
+            <br />
+
+            <asp:Label ID="QtyLbl" runat="server" Text="Quantity"></asp:Label>
+            <asp:TextBox ID="QtyTextbox" runat="server" CssClass="form-control"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="QtyReq" runat="server" ErrorMessage="Quantity is required" Display="Dynamic" ControlToValidate="QtyTextBox" ></asp:RequiredFieldValidator>            
+
+            <asp:Button ID="AddToCart" runat="server" Text="Add To Cart" CssClass="btn btn-primary" OnClick="AddToCart_Click" />
+        </div>
+
+        <div class="col-md-6">
+            <asp:Label ID="CartLbl" runat="server" Text="Cart"></asp:Label>
+            <asp:Table ID="Table1" runat="server" CssClass="table"></asp:Table>
+        </div>
+    </div>
 </asp:Content>
 <asp:Content ID="Content4" ContentPlaceHolderID="footer" Runat="Server">
 </asp:Content>
--- final proj/ABCHardware/ABCHardwareSite/ABCHardwareSite/ProcessSale.aspx.cs
+++ final proj/ABCHardware/ABCHardwareSite/ABCHardwareSite/ProcessSale.aspx.cs
@@ -9,6 +9,91 @@ public partial class ProcessSale : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        TableHeaderRow thr = new TableHeaderRow();
 
+        TableHeaderCell thc1 = new TableHeaderCell();
+        thc1.Text = "Item Code";
+        TableHeaderCell thc2 = new TableHeaderCell();
+        thc2.Text = "Quantity in cart";
+
+        thr.Cells.Add(thc1);
+        thr.Cells.Add(thc2);
+
+        Table1.Rows.Add(thr);
+        ReloadTable();
+    }
+
+    private void ReloadTable()
+    {
+        Dictionary<string, int> values = (Dictionary<string, int>)Session["cart"];
+        if (values != null)
+        {
+            
+            foreach (KeyValuePair<string, int> item in values)
+            {
+                TableRow tr = new TableRow();
+                TableCell tc1 = new TableCell();
+                tc1.Text = item.Key;
+
+                tr.Cells.Add(tc1);
+
+                TableCell tc2 = new TableCell();
+                tc2.Text = item.Value + "";
+
+                tr.Cells.Add(tc2);
+                Table1.Rows.Add(tr);
+            }
+        }
+    }
+
+    protected void AddToCart_Click(object sender, EventArgs e)
+    {
+        // if item exists, add qty to cart
+        ABCController controller = new ABCController();
+        bool found = false;
+        FormTools ft = new FormTools();
+        if (IsValid)
+        {
+            try
+            {
+                Item item = controller.LookupItem(ItemCodeTextBox.Text);
+                if (!string.IsNullOrEmpty(item.ItemCode))
+                {
+                    found = true;
+                    Dictionary<string, int> values = (Dictionary<string, int>)Session["cart"];
+                    if (values != null)
+                    {
+                        int qty;
+                        if (values.TryGetValue(item.ItemCode, out qty))
+                        {
+                            values[item.ItemCode] += int.Parse(QtyTextbox.Text);
+                        }
+                        else
+                        {
+                            values[item.ItemCode] = int.Parse(QtyTextbox.Text);
+                        }
+                    }
+                    else
+                    {
+                        values = new Dictionary<string, int>();
+                        values[item.ItemCode] = int.Parse(QtyTextbox.Text);
+                        Session["cart"] = values;                        
+                    }
+                    ReloadTable();
+                }
+                else
+                {                    
+                    ft.MessageBox(MessageBox, "Item not found", false);
+                }
+            }
+            catch (Exception)
+            {
+                ft.MessageBox(MessageBox, "Item lookup unsuccessful, not added to cart", false);
+            }
+        }
+        if (!found)
+        {
+            ft.MessageBox(MessageBox, "Item lookup unsuccessful, not added to cart", false);
+        }
     }
 }
